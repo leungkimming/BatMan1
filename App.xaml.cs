fe06d9f8d2153ba2cc02474df412a1f495c324e2
kmleung <kmleung@live.com>
2024-06-14 21:06:50 +0800

    build_deploy_success

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BatMan2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BatMan2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
